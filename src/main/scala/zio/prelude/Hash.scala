28af28c9c83fb9617ebda725465ee24320f16a11
sken <sken77@pm.me>
2020-03-05 18:35:08 -0800

    add Hash implicits

--- src/main/scala/zio/prelude/Hash.scala
+++ src/main/scala/zio/prelude/Hash.scala
@@ -1,28 +1,58 @@
 package zio.prelude
 
-trait HashLaws[-A] {
-  def hash(a: A): Int
-
-  final def consistencyLaw[A1 <: A](a1: A1, a2: A1)(implicit equal: Equal[A1]): Boolean =
-    (a1 === a2) ==> (hash(a1) === hash(a2))
+/**
+ * `Hash[A]` provides implicit evidence that a value of type `A` hash a Hash(`A`)
+ */
+sealed trait Hash[-A] { self =>
+  def hash (a: A): Int
 }
-sealed trait Hash[-A] extends HashLaws[A]
-object Hash {
-  def apply[A](implicit hash: Hash[A]): Hash[A] = hash
 
-  def apply[A](f: A => Int): Hash[A] =
+object Hash {
+  def apply[A] (implicit hash: Hash[A]): Hash[A] = hash
+
+  def apply[A] (f: A => Int): Hash[A] =
     new Hash[A] {
-      def hash(a: A): Int = f(a)
+      def hash (a: A): Int = f(a)
     }
 
   def default[A]: Hash[A] = Hash[A]((a: A) => a.hashCode)
 
+  implicit val booleanHash: Hash[Boolean] = Hash.default[Boolean]
+
+  implicit val byteHash: Hash[Byte] = Hash.default[Byte]
+
+  implicit val charHash: Hash[Char] = Hash.default[Char]
+
+  implicit val doubleHash: Hash[Double] = Hash.default[Double]
+
+  implicit def eitherHash[L: Hash, R: Hash]: Hash[Either[L, R]] = Hash.default[Either[L, R]]
+
+  implicit val floatHash: Hash[Float] = Hash.default[Float]
+
+  implicit val intHash: Hash[Int] = Hash.default[Int]
+
+  implicit def listHash[A: Hash]: Hash[List[A]] = Hash.default[List[A]]
+
+  implicit val longHash: Hash[Long] = Hash.default[Long]
+
+  implicit def mapHash[K: Hash, V: Hash]: Hash[Map[K, V]] = Hash.default[Map[K, V]]
+
+  implicit def optionHash[A: Hash]: Hash[Option[A]] = Hash.default[Option[A]]
+
+  implicit def setHash[A: Hash]: Hash[Set[A]] = Hash.default[Set[A]]
+
   implicit val stringHash: Hash[String] = Hash.default[String]
-  implicit val intHash: Hash[Int]       = Hash.default[Int]
-  implicit val longHash: Hash[Long]     = Hash.default[Long]
+
+  implicit val unitHash: Hash[Unit] = Hash.default[Unit]
+
+  implicit def vectorHash[A: Hash]: Hash[Vector[A]] = Hash.default[Vector[A]]
 }
+
 trait HashSyntax {
-  implicit class HashSyntax[A](a: A) {
-    def hash(implicit hash: Hash[A]): Int = hash.hash(a)
+
+  implicit class HashSyntax[A] (a: A) {
+    def hash (implicit hash: Hash[A]): Int = hash.hash(a)
+
+    def ## (implicit hash: Hash[A]): Int = hash.hash(a)
   }
 }
---
+++ src/test/scala/zio/prelude/HashSpec.scala
@@ -0,0 +1,33 @@
+package zio.prelude
+
+import zio.ZIO
+import zio.test._
+import zio.test.Assertion._
+import zio.test.DefaultRunnableSpec
+
+object HashSpec extends DefaultRunnableSpec {
+  final def consistencyLaw[R, A: Hash : Equal] (gen: Gen[R, A]): ZIO[R, Nothing, TestResult] =
+    check(gen, gen) { (a1, a2) =>
+      assert((a1 === a2) ==> (a1.## === a2.##))(isTrue ?? "consistencyLaw")
+    }
+
+  def spec = suite("HashSpec")(
+    suite("laws")(
+      testM("boolean")(consistencyLaw(Gen.boolean)),
+      testM("byte")(consistencyLaw(Gen.anyByte)),
+      testM("char")(consistencyLaw(Gen.anyChar)),
+      testM("double")(consistencyLaw(Gen.anyDouble)),
+      testM("either")(consistencyLaw(Gen.either(Gen.anyInt, Gen.anyInt))),
+      testM("float")(consistencyLaw(Gen.anyFloat)),
+      testM("int")(consistencyLaw(Gen.anyInt)),
+      testM("list")(consistencyLaw(Gen.listOf(Gen.anyInt))),
+      testM("long")(consistencyLaw(Gen.anyLong)),
+      testM("map")(consistencyLaw(TestUtil.anyMap(Gen.anyInt, Gen.anyInt))),
+      testM("option")(consistencyLaw(Gen.option(Gen.anyInt))),
+      testM("set")(consistencyLaw(TestUtil.anySet(Gen.anyInt))),
+      testM("string")(consistencyLaw(Gen.anyString)),
+      testM("unit")(consistencyLaw(Gen.unit)),
+      testM("vector")(consistencyLaw(Gen.vectorOf(Gen.anyInt)))
+      )
+    )
+}
