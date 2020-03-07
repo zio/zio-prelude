ce75acb50136687cdc10856621461a06bfeb4756
John A. De Goes <john@degoes.net>
2020-03-06 22:06:30 -0500

    Add to hash a few helper methods

--- src/main/scala/zio/prelude/Hash.scala
+++ src/main/scala/zio/prelude/Hash.scala
@@ -6,7 +6,28 @@ trait HashLaws[-A] {
   final def consistencyLaw[A1 <: A](a1: A1, a2: A1)(implicit equal: Equal[A1]): Boolean =
     (a1 === a2) <==> (hash(a1) === hash(a2))
 }
-sealed trait Hash[-A] extends HashLaws[A]
+sealed trait Hash[-A] extends HashLaws[A] { self =>
+  def both[B](that: Hash[B]): Hash[(A, B)] = (self bothWith that)(t => t._1 -> t._2)
+
+  def bothWith[B, C](that: Hash[B])(f: C => (A, B)): Hash[C] =
+    Hash { (c: C) =>
+      // TODO: Use Tuple hash when it exists
+      val (a, b) = f(c)
+
+      java.util.Arrays.hashCode(Array(self.hash(a), that.hash(b)))
+    }
+
+  def either[B](that: Hash[B]): Hash[Either[A, B]] = (self eitherWith that)(identity)
+
+  def eitherWith[B, C](that: Hash[B])(f: C => Either[A, B]): Hash[C] =
+    Hash { (c: C) =>
+      // TODO: Use Either hash when it exists
+      f(c) match {
+        case Left(a)  => java.util.Arrays.hashCode(Array("Left".hashCode, self.hash(a)))
+        case Right(b) => java.util.Arrays.hashCode(Array("Right".hashCode, that.hash(b)))
+      }
+    }
+}
 object Hash {
   def apply[A](implicit hash: Hash[A]): Hash[A] = hash
 
