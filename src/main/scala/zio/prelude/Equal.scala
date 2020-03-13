dd58c9e7091ee0e16654d9df995df0921c2e9722
John A. De Goes <john@degoes.net>
2020-03-13 17:57:35 -0400

    cleanup

--- src/main/scala/zio/prelude/Equal.scala
+++ src/main/scala/zio/prelude/Equal.scala
@@ -5,26 +5,6 @@ import scala.annotation.implicitNotFound
 import zio.test.TestResult
 import zio.test.laws.{ Lawful, Laws }
 
-trait EqualLaws extends Lawful[Equal] {
-
-  final val reflexiveLaw = new Laws.Law1[Equal]("reflexiveLaw") {
-    def apply[A: Equal](a1: A): TestResult =
-      a1 <-> a1
-  }
-
-  final val symmetryLaw = new Laws.Law2[Equal]("symmetryalaw") {
-    def apply[A: Equal](a1: A, a2: A): TestResult =
-      (a1 === a2) ==> (a2 === a1)
-  }
-
-  final val transitivityLaw = new Laws.Law3[Equal]("transitivityLaw") {
-    def apply[A: Equal](a1: A, a2: A, a3: A): TestResult =
-      ((a1 === a2) && (a2 === a3)) ==> (a1 === a3)
-  }
-
-  final val laws = reflexiveLaw + symmetryLaw + transitivityLaw
-}
-
 /**
  * `Equal[A]` provides implicit evidence that two values of type `A` can be
  * compared for equality.
@@ -95,7 +75,23 @@ sealed trait Equal[-A] { self =>
   final def notEqual(l: A, r: A): Boolean = !equal(l, r)
 }
 
-object Equal extends EqualLaws {
+object Equal extends Lawful[Equal] {
+  final val reflexiveLaw = new Laws.Law1[Equal]("reflexiveLaw") {
+    def apply[A: Equal](a1: A): TestResult =
+      a1 <-> a1
+  }
+
+  final val symmetryLaw = new Laws.Law2[Equal]("symmetryLaw") {
+    def apply[A: Equal](a1: A, a2: A): TestResult =
+      (a1 <-> a2) ==> (a2 <-> a1)
+  }
+
+  final val transitivityLaw = new Laws.Law3[Equal]("transitivityLaw") {
+    def apply[A: Equal](a1: A, a2: A, a3: A): TestResult =
+      ((a1 <-> a2) && (a2 <-> a3)) ==> (a1 <-> a3)
+  }
+
+  final val laws = reflexiveLaw + symmetryLaw + transitivityLaw
 
   /**
    * Summons an implicit `Equal[A]`.
