97cb90b874d371973a3db2647a3f666278208ab8
Dejan Mijic <dmijic@acm.org>
2020-03-28 15:01:54 +0100

    Add coherency test for IdentityEqual

--- src/test/scala/zio/prelude/coherent/CoherentSpec.scala
+++ src/test/scala/zio/prelude/coherent/CoherentSpec.scala
@@ -2,14 +2,22 @@ package zio.prelude.coherent
 
 import zio.prelude._
 import zio.test._
+import zio.test.Assertion.{ isEmptyString, isTrue }
 
 object CoherentSpec extends DefaultRunnableSpec {
 
   def spec = suite("CoherentSpec")(
-    test("intersections can be derived") {
+    test("HashOrd") {
       val instance = implicitly[Hash[Int] with Ord[Int]]
       assert(instance.hash(0))(equalTo(0)) &&
       assert(instance.compare(0, 1))(equalTo(Ordering.LessThan))
+    },
+    test("IdentityEqual") {
+      val instance = implicitly[Identity[String] with Equal[String]]
+
+      assert(instance.identity)(isEmptyString) &&
+      assert(instance.combine("a", "b"))(equalTo("ab")) &&
+      assert(instance.equal("a", "a"))(isTrue)
     }
   )
 }
