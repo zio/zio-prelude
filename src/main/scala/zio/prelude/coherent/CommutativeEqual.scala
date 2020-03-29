78941c3afd615414737d11a5a013125a78159787
Landlocked Surfer <landlocked.surfer@yahoo.com>
2020-03-29 23:11:36 +0200

    Add Commutative instances for all standard Scala types and their newtypes (#53)
    * Ord fix
    
    * Add Commutative instances

---
+++ src/main/scala/zio/prelude/coherent/CommutativeEqual.scala
@@ -0,0 +1,19 @@
+package zio.prelude.coherent
+
+import zio.prelude._
+
+trait CommutativeEqual {
+
+  /**
+   * Derives a `Commutative[A] with Equal[A]` given a `Commutative[A]` and an `Equal[A]`.
+   */
+  implicit def commutativeEqual[A](
+    implicit commutative0: Commutative[A],
+    equal0: Equal[A]
+  ): Commutative[A] with Equal[A] =
+    new Commutative[A] with Equal[A] {
+      override def combine(l: A, r: A): A = commutative0.combine(l, r)
+
+      override protected def checkEqual(l: A, r: A): Boolean = equal0.equal(l, r)
+    }
+}
