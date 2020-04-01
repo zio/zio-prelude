b3a11d36a6d5571ee44b1ecdf9fb6a133171c30d
sken <sken77@pm.me>
2020-04-01 14:05:18 -0700

    add case class

--- src/test/scala/zio/prelude/DebugSpec.scala
+++ src/test/scala/zio/prelude/DebugSpec.scala
@@ -1,5 +1,6 @@
 package zio.prelude
 
+import zio.prelude.Debug.Repr
 import zio.test._
 
 object DebugSpec extends DefaultRunnableSpec {
@@ -7,12 +8,20 @@ object DebugSpec extends DefaultRunnableSpec {
   def primitiveTest[A: Debug](a: A, exp: Option[String] = None) =
     assert(a.debug.render())(equalTo(exp.getOrElse(a.toString)))
 
-  case class TestCase(string: String, number: Int, list: List[String])
+  case class TestCase(string: String, number: Int, list: List[Double])
   val genTestCase = for {
     str    <- Gen.anyString
     number <- Gen.anyInt
-    list   <- Gen.listOf(Gen.anyString)
+    list   <- Gen.listOf(Gen.anyDouble)
   } yield TestCase(str, number, list)
+
+  implicit val DebugTestCase = Debug.make[TestCase](a =>
+    Repr.Constructor(
+      List("DebugSpec"),
+      "TestCase",
+      Map("string" -> a.string.debug, "number" -> a.number.debug, "list" -> a.list.debug)
+    )
+  )
 
   def spec = suite("DebugSpec")(
     testM("unit")(check(Gen.unit)(primitiveTest(_, Some("scala.()")))),
@@ -23,6 +32,14 @@ object DebugSpec extends DefaultRunnableSpec {
     testM("byte")(check(Gen.anyByte)(primitiveTest(_))),
     testM("char")(check(Gen.anyChar)(primitiveTest(_))),
     testM("string")(check(Gen.anyString)(s => primitiveTest(s, Some(s""""$s"""")))),
-    testM("list")(check(Gen.listOf(Gen.anyInt))(c => assert(c.debug.render())(equalTo(s"scala.${c.toString}"))))
+    testM("list")(check(Gen.listOf(Gen.anyInt))(c => assert(c.debug.render())(equalTo(s"scala.${c.toString}")))),
+    testM("caseclass")(
+      check(genTestCase)(c =>
+        assert(c.debug.render()) {
+          val str = s""""${c.string}""""
+          equalTo(s"DebugSpec.TestCase(string -> $str, number -> ${c.number}, list -> scala.${c.list})")
+        }
+      )
+    )
   )
 }
