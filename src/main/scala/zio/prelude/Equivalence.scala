edecfa78fb8183ea2a6780569688f27472d07a25
John A. De Goes <john@degoes.net>
2020-04-09 12:22:52 -0400

    A few helper methods for the equivalences.

--- src/main/scala/zio/prelude/Equivalence.scala
+++ src/main/scala/zio/prelude/Equivalence.scala
@@ -1,5 +1,18 @@
 package zio.prelude
 
-final case class Equivalence[A, B](to: A => B, from: B => A) {
+final case class Equivalence[A, B](to: A => B, from: B => A) { self =>
+  def >>>[C](that: Equivalence[B, C]): Equivalence[A, C] = self andThen that
+
+  def andThen[C](that: Equivalence[B, C]): Equivalence[A, C] =
+    Equivalence(self.to andThen that.to, self.from compose that.from)
+
+  def compose[C](that: Equivalence[C, A]): Equivalence[C, B] = that andThen self
+
   def flip: Equivalence[B, A] = Equivalence(from, to)
+
+  def toPartialEquivalence: PartialEquivalence[A, B, Nothing, Nothing] =
+    PartialEquivalence((a: A) => Right(to(a)), (b: B) => Right(from(b)))
+}
+object Equivalence {
+  def identity[A]: Equivalence[A, A] = Equivalence(Predef.identity[A](_), Predef.identity[A](_))
 }
--- src/main/scala/zio/prelude/PartialEquivalence.scala
+++ src/main/scala/zio/prelude/PartialEquivalence.scala
@@ -1,10 +1,34 @@
 package zio.prelude
 
-final case class PartialEquivalence[A, B, +E1, +E2](to: A => Either[E1, B], from: B => Either[E2, A]) {
-  def canonicalLeft(a: A): Option[A] = to(a).toOption.flatMap(b => from(b).toOption)
+final case class PartialEquivalence[A, B, +E1, +E2](to: A => Either[E1, B], from: B => Either[E2, A]) { self =>
+  import PartialEquivalence._
+  def >>>[C, E3 >: E1, E4 >: E2](that: PartialEquivalence[B, C, E3, E4]): PartialEquivalence[A, C, E3, E4] =
+    self andThen that
 
-  def canonicalRight(b: B): Option[B] = from(b).toOption.flatMap(a => to(a).toOption)
+  def andThen[C, E3 >: E1, E4 >: E2](that: PartialEquivalence[B, C, E3, E4]): PartialEquivalence[A, C, E3, E4] =
+    PartialEquivalence(self.to andThen (_.flatMap(that.to)), that.from andThen (_.flatMap(self.from)))
+
+  def canonicalLeft(a: A): Option[A] = canonicalLeftOrError[Any](a).toOption
+
+  def canonicalLeftOrError[E](a: A)(implicit ev1: E1 <:< E, ev2: E2 <:< E): Either[E, A] =
+    to(a).left.map(ev1).flatMap(b => from(b).left.map(ev2))
+
+  def canonicalRight(b: B): Option[B] = canonicalRightOrError[Any](b).toOption
+
+  def canonicalRightOrError[E](b: B)(implicit ev1: E1 <:< E, ev2: E2 <:< E): Either[E, B] =
+    from(b).left.map(ev2).flatMap(a => to(a).left.map(ev1))
+
+  def compose[C, E3 >: E1, E4 >: E2](that: PartialEquivalence[C, A, E3, E4]): PartialEquivalence[C, B, E3, E4] =
+    that andThen self
+
+  def flip: PartialEquivalence[B, A, E2, E1] = PartialEquivalence(from, to)
+
+  def toEquivalence(implicit ev1: E1 <:< Nothing, ev2: E2 <:< Nothing): Equivalence[A, B] =
+    Equivalence((a: A) => toRight(to(a).left.map(ev1)), (b: B) => toRight(from(b).left.map(ev2)))
 }
 
-// TODO?
-// type Equivalence[A, B] = PartialEquivalence[A, B, Nothing, Nothing]
+object PartialEquivalence {
+  def identity[A]: PartialEquivalence[A, A, Nothing, Nothing] = Equivalence.identity[A].toPartialEquivalence
+
+  private def toRight[A](either: Either[Nothing, A]): A = either.fold(Predef.identity, Predef.identity)
+}
