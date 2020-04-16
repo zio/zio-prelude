515151b9bfefbb03b46ec6992af4c9fdc13a943f
Kamal Kang <kamal.k.kang@gmail.com>
2020-04-16 11:29:41 -0400

    zio-prelude#80/81 - Add Invariant instances for Scala and Zio Prelude Types (#90)
    * zio-prelude#80/81 - Add Invariant instances for Scala and Zio Prelude types
    
    * zio-prelude#80/81 - Add Invariant instances for Inverse and instances for Inverse
    
    * zio-prelude#80/81 - Fixed inverse instances and tests. Removed Prod types for now.
    
    Co-authored-by: k_kang <k_kang@conversantmedia.com>

---
+++ src/test/scala/zio/prelude/InverseSpec.scala
@@ -0,0 +1,18 @@
+package zio.prelude
+
+import zio.prelude.newtypes.{ Sum }
+import zio.test._
+import zio.test.laws._
+import zio.test.DefaultRunnableSpec
+
+object InverseSpec extends DefaultRunnableSpec {
+  def spec = suite("InverseSpec")(
+    suite("laws")(
+      testM("char addition")(checkAllLaws(Inverse)(Gen.anyChar.map(Sum(_)))),
+      testM("byte addition")(checkAllLaws(Inverse)(Gen.anyByte.map(Sum(_)))),
+      testM("short addition")(checkAllLaws(Inverse)(Gen.anyShort.map(Sum(_)))),
+      testM("int addition")(checkAllLaws(Inverse)(Gen.anyInt.map(Sum(_)))),
+      testM("long addition")(checkAllLaws(Inverse)(Gen.anyLong.map(Sum(_))))
+    )
+  )
+}
