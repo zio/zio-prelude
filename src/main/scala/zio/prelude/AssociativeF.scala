63d7a5f409033de7f86f004f40e4aac4a6f1100c
John A. De Goes <john@degoes.net>
2020-04-17 07:43:52 -0400

    Laziness

--- src/main/scala/zio/prelude/AssociativeF.scala
+++ src/main/scala/zio/prelude/AssociativeF.scala
@@ -2,7 +2,7 @@ package zio.prelude
 
 object AssociativeF {
   trait Both[F[_]] {
-    def both[A, B](fa: F[A], fb: F[B]): F[(A, B)]
+    def both[A, B](fa: => F[A], fb: => F[B]): F[(A, B)]
 
     def associativityLaw[A: Equal, B: Equal, C: Equal](fa: F[A], fb: F[B], fc: F[C])(
       implicit invariant: Invariant[F],
@@ -19,7 +19,7 @@ object AssociativeF {
   object Both {
     implicit val BothOption: Both[Option] =
       new Both[Option] {
-        def both[A, B](fa: Option[A], fb: Option[B]): Option[(A, B)] =
+        def both[A, B](fa: => Option[A], fb: => Option[B]): Option[(A, B)] =
           (fa, fb) match {
             case (Some(a), Some(b)) => Some((a, b))
             case _                  => None
@@ -28,7 +28,7 @@ object AssociativeF {
   }
 
   trait Either[F[_]] {
-    def either[A, B](fa: F[A], fb: F[B]): F[scala.util.Either[A, B]]
+    def either[A, B](fa: => F[A], fb: => F[B]): F[scala.util.Either[A, B]]
 
     def associativityLaw[A: Equal, B: Equal, C: Equal](fa: F[A], fb: F[B], fc: F[C])(
       implicit invariant: Invariant[F],
@@ -42,21 +42,28 @@ object AssociativeF {
       left2 === right
     }
   }
+  object Either {
+    implicit val EitherOption: Either[Option] =
+      new Either[Option] {
+        def either[A, B](fa: => Option[A], fb: => Option[B]): Option[scala.util.Either[A, B]] =
+          fa.map(Left(_)) orElse fb.map(Right(_))
+      }
+  }
 }
 
 trait AssociativeFSyntax {
-  implicit class AssociativeFOps[F[_], A](fa: F[A]) {
-    def zip[B](fb: F[B])(implicit both: AssociativeF.Both[F]): F[(A, B)] =
+  implicit class AssociativeFOps[F[_], A](fa: => F[A]) {
+    def zip[B](fb: => F[B])(implicit both: AssociativeF.Both[F]): F[(A, B)] =
       both.both(fa, fb)
 
-    def orElseEither[B](fb: F[B])(implicit either: AssociativeF.Either[F]): F[Either[A, B]] =
+    def orElseEither[B](fb: => F[B])(implicit either: AssociativeF.Either[F]): F[Either[A, B]] =
       either.either(fa, fb)
   }
-  implicit class AssociativeFCovariantOps[F[+_], A](fa: F[A]) {
-    def zipWith[B, C](fb: F[B])(f: (A, B) => C)(implicit both: AssociativeF.Both[F], covariant: Covariant[F]): F[C] =
+  implicit class AssociativeFCovariantOps[F[+_], A](fa: => F[A]) {
+    def zipWith[B, C](fb: => F[B])(f: (A, B) => C)(implicit both: AssociativeF.Both[F], covariant: Covariant[F]): F[C] =
       both.both(fa, fb).map(f.tupled)
 
-    def orElse(fa2: F[A])(implicit either: AssociativeF.Either[F], covariant: Covariant[F]): F[A] =
+    def orElse(fa2: => F[A])(implicit either: AssociativeF.Either[F], covariant: Covariant[F]): F[A] =
       either.either(fa, fa2).map(_.merge)
   }
 }
