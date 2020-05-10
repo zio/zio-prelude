e1ba33b78016ea56be304a7958872d5c7f022796
Jorge Aliss <jaliss@gmail.com>
2020-05-10 19:12:02 -0300

    Added tests

--- src/main/scala/zio/prelude/GenFs.scala
+++ src/main/scala/zio/prelude/GenFs.scala
@@ -21,6 +21,48 @@ object GenFs {
         Gen.option(gen)
     }
 
+  val list: GenF[Random with Sized, List] =
+    new GenF[Random with Sized, List] {
+      def apply[R1 <: Random with Sized, A](gen: Gen[R1, A]): Gen[R1, List[A]] =
+        Gen.listOf(gen)
+    }
+
+  val vector: GenF[Random with Sized, Vector] =
+    new GenF[Random with Sized, Vector] {
+      def apply[R1 <: Random with Sized, A](gen: Gen[R1, A]): Gen[R1, Vector[A]] =
+        Gen.vectorOf(gen)
+    }
+
+  val set: GenF[Random with Sized, Set] =
+    new GenF[Random with Sized, Set] {
+      def apply[R1 <: Random with Sized, A](gen: Gen[R1, A]): Gen[R1, Set[A]] =
+        Gen.setOf(gen)
+    }
+
+  def tuple2[R <: Random with Sized, A](a: Gen[R, A]): GenF[R, ({ type lambda[+x] = (A, x) })#lambda] =
+    new GenF[R, ({ type lambda[+x] = (A, x) })#lambda] {
+      def apply[R1 <: R, B](b: Gen[R1, B]): Gen[R1, (A, B)] =
+        a.zip(b)
+    }
+
+  def tuple3[R <: Random with Sized, A, B](a: Gen[R, (A, B)]): GenF[R, ({ type lambda[+x] = ((A, B), x) })#lambda] =
+    new GenF[R, ({ type lambda[+x] = ((A, B), x) })#lambda] {
+      def apply[R1 <: R, C](b: Gen[R1, C]): Gen[R1, ((A, B), C)] =
+        a.zip(b)
+    }
+
+  def map[R <: Random with Sized, K](k: Gen[R, K]): GenF[R, ({ type lambda[+x] = Map[K, x] })#lambda] =
+    new GenF[R, ({ type lambda[+x] = Map[K, x] })#lambda] {
+      def apply[R1 <: R, V](v: Gen[R1, V]): Gen[R1, Map[K, V]] =
+        Gen.mapOf(k, v)
+    }
+
+  def either[R <: Random with Sized, A](e: Gen[R, A]): GenF[R, ({ type lambda[+x] = Either[A, x] })#lambda] =
+    new GenF[R, ({ type lambda[+x] = Either[A, x] })#lambda] {
+      def apply[R1 <: R, B](a: Gen[R1, B]): Gen[R1, Either[A, B]] =
+        Gen.either(e, a)
+    }
+
   /**
    * A generator of validation values.
    */
--- src/test/scala/zio/prelude/CovariantSpec.scala
+++ src/test/scala/zio/prelude/CovariantSpec.scala
@@ -1,13 +1,32 @@
 package zio.prelude
 
-import zio.test._
+import zio.random.Random
+import zio.test.{ Gen, _ }
 import zio.test.laws._
 
 object CovariantSpec extends DefaultRunnableSpec {
 
+  val genFMap: GenF[Random with Sized, ({ type lambda[+x] = Map[Int, x] })#lambda] =
+    GenFs.map(Gen.anyInt)
+
+  val genFEither: GenF[Random with Sized, ({ type lambda[+x] = Either[Int, x] })#lambda] =
+    GenFs.either(Gen.anyInt)
+
+  val genFTuple: GenF[Random with Sized, ({ type lambda[+x] = (Int, x) })#lambda] =
+    GenFs.tuple2(Gen.anyInt)
+
+  val genFTuple3: GenF[Random with Sized, ({ type lambda[+x] = ((Int, Int), x) })#lambda] =
+    GenFs.tuple3(Gen.anyInt.zip(Gen.anyInt))
+
   def spec = suite("CovariantSpec")(
     suite("laws")(
-      testM("option")(checkAllLaws(Covariant)(GenFs.option, Gen.anyInt))
+      testM("option")(checkAllLaws(Covariant)(GenFs.option, Gen.anyInt)),
+      testM("list")(checkAllLaws(Covariant)(GenFs.list, Gen.anyInt)),
+      testM("vector")(checkAllLaws(Covariant)(GenFs.vector, Gen.anyInt)),
+      testM("map")(checkAllLaws(Covariant)(genFMap, Gen.anyInt)),
+      testM("either")(checkAllLaws(Covariant)(genFEither, Gen.anyInt)),
+      testM("tuple2")(checkAllLaws(Covariant)(genFTuple, Gen.anyInt)),
+      testM("tuple3")(checkAllLaws(Covariant)(genFTuple3, Gen.anyInt))
     )
   )
 }
