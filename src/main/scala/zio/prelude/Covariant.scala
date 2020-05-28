8662cbd456c23519552df6616065c482a08b0f4c
Jorge Aliss <jaliss@gmail.com>
2020-05-28 17:44:05 -0300

    Resolves #125: Add `Covariant` instance for a failed Either (#126)

--- src/main/scala/zio/prelude/Covariant.scala
+++ src/main/scala/zio/prelude/Covariant.scala
@@ -135,6 +135,21 @@ object Covariant extends LawfulF.Covariant[CovariantEqualF, Equal] {
     }
 
   /**
+   * The `Covariant` instance for a failed `Either`
+   */
+  implicit def EitherFailedCovariant[R]: Covariant[({ type lambda[+l] = Failure[Either[l, R]] })#lambda] =
+    new Covariant[({ type lambda[+l] = Failure[Either[l, R]] })#lambda] {
+      override def map[L, L1](f: L => L1): Failure[Either[L, R]] => Failure[Either[L1, R]] = { either =>
+        Failure.wrap {
+          Failure.unwrap(either) match {
+            case Left(l)  => Left[L1, R](f(l))
+            case Right(r) => Right[L1, R](r)
+          }
+        }
+      }
+    }
+
+  /**
    * The `Covariant` instance for `Try`
    */
   implicit val TryCovariant: Covariant[Try] =
