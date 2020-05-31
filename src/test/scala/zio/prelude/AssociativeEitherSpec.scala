3d9939b4f1e0d7a8cd01de9b0665f39e28929123
Dejan Mijić <dmijic@acm.org>
2020-06-01 01:11:53 +0200

    Add AssociativeEither instances for Either, Try and Future (#146)
    * Add AssociativeEither instance for Try
    
    * Add AssociativeEither instance for Future
    
    * Add Invariant instance for Either
    
    * Add AssociativeEither instance(s) for Either

--- src/test/scala/zio/prelude/AssociativeEitherSpec.scala
+++ src/test/scala/zio/prelude/AssociativeEitherSpec.scala
@@ -2,12 +2,17 @@ package zio.prelude
 
 import zio.test._
 import zio.test.laws._
+import zio.random.Random
 
 object AssociativeEitherSpec extends DefaultRunnableSpec {
 
+  val genFEither: GenF[Random with Sized, ({ type lambda[+x] = Either[Int, x] })#lambda] =
+    GenFs.either(Gen.anyInt)
+
   def spec = suite("AssociativeEitherSpec")(
     suite("laws")(
-      testM("option")(checkAllLaws(AssociativeEither)(GenFs.option, Gen.anyInt))
+      testM("option")(checkAllLaws(AssociativeEither)(GenFs.option, Gen.anyInt)),
+      testM("either")(checkAllLaws(AssociativeEither)(genFEither, Gen.anyInt))
     )
   )
 }
