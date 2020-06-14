f7f7e8609826fc5bfdb1b2ef1e4a0ba1369f0202
Adam Fraser <adam.fraser@gmail.com>
2020-06-14 10:54:37 -0700

    try to isolate CI issue

--- src/test/scala/zio/prelude/TraversableSpec.scala
+++ src/test/scala/zio/prelude/TraversableSpec.scala
@@ -1,15 +1,15 @@
-// package zio.prelude
+package zio.prelude
 
-// import zio.test._
+import zio.test._
 
-// object TraversableSpec extends DefaultRunnableSpec {
+object TraversableSpec extends DefaultRunnableSpec {
 
-//   def spec = suite("TraversableSpec")(
-//     test("zipWithIndex is stacks safe") {
-//       val as       = (1 to 100000).toList
-//       val expected = as.zipWithIndex
-//       val actual   = Traversable.ListTraversable.zipWithIndex(as)
-//       assert(actual)(equalTo(expected))
-//     }
-//   )
-// }
+  def spec = suite("TraversableSpec")(
+    test("zipWithIndex is stacks safe") {
+      val as       = (1 to 100000).toList
+      val expected = as.zipWithIndex
+      val actual   = Traversable.ListTraversable.zipWithIndex(as)
+      assert(actual)(equalTo(expected))
+    }
+  )
+}
