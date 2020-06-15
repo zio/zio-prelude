2e739ccae3669a90a969e79472aa33ed90c9d1c7
John A. De Goes <john@degoes.net>
2020-06-15 16:55:48 -0400

    Rename

--- src/main/scala/zio/prelude/CCC.scala
+++ src/main/scala/zio/prelude/CCC.scala
@@ -7,7 +7,7 @@ trait CCC[:=>[-_, +_]] extends IdentityCompose[:=>] {
   def first[A, B, C](f: A :=> (B, C)): A :=> B
   def second[A, B, C](f: A :=> (B, C)): A :=> C
 
-  def swapBoth[A, B]: (A, B) :=> (B, A)
+  def flipBoth[A, B]: (A, B) :=> (B, A)
 
   def lassocBoth[A, B, C]: (A, (B, C)) :=> ((A, B), C)
   def rassocBoth[A, B, C]: ((A, B), C) :=> (A, (B, C))
@@ -16,7 +16,7 @@ trait CCC[:=>[-_, +_]] extends IdentityCompose[:=>] {
   def left[A, B, C](f: Either[A, B] :=> C): A :=> C
   def right[A, B, C](f: Either[A, B] :=> C): B :=> C
 
-  def swapEither[A, B]: Either[A, B] :=> Either[B, A]
+  def flipEither[A, B]: Either[A, B] :=> Either[B, A]
 
   def lassocEither[A, B, C]: Either[A, Either[B, C]] :=> Either[Either[A, B], C]
   def rassocEither[A, B, C]: Either[Either[A, B], C] :=> Either[A, Either[B, C]]
