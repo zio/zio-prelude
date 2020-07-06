bb76fd45f1323b35aa509ba9e2e00cc15ffc63fc
Kamal Kang <kamal.k.kang@gmail.com>
2020-07-06 13:14:03 -0400

    Added AssociativeFlatten and IdentityFlatten Instances. (#156)
    * zio-prelude#80/81 - Add Invariant instances for Scala and Zio Prelude types
    
    * zio-prelude#80/81 - Add Invariant instances for Inverse and instances for Inverse
    
    * zio-prelude#80/81 - Fixed inverse instances and tests. Removed Prod types for now.
    
    * make state stack safe
    
    * format
    
    * implement State#suspend
    
    * add documentation
    
    * add convenience methods
    
    * more combinators and instances
    
    * add tests
    
    * investigate CI issues
    
    * enable State tests
    
    * zio-prelude - 136,138,139,140,141,142
    - Added  AssociativeBoth instances
    - Added  CommutativeBoth instances
    - Added  CommutativeEither instances
    
    * try to isolate CI issue
    
    * avoid lazy val
    
    * Update src/main/scala/zio/prelude/AssociativeBoth.scala
    
    Co-authored-by: Adam Fraser <adam.fraser@gmail.com>
    
    * zio-prelude - 136,138,139,140,141,142
    - Code review recommendations
    
    * zio-prelude - 136,138,139,140,141,142
    - Code review recommendations
    
    * zio-prelude - 136,138,139,140,141,142
    - Code review recommendations
    
    * Merge remote-tracking branch 'upstream/master'
    
    # Conflicts:
    #	src/main/scala/zio/prelude/Traversable.scala
    
    * Deleted old file, StateSpec
    
    * Fixed Scala 2.13 deprecation issue
    
    * Added AssociativeFlatten and IdentityFlatten instances
    
    Co-authored-by: k_kang <k_kang@conversantmedia.com>
    Co-authored-by: Adam Fraser <adam.fraser@gmail.com>

--- src/main/scala/zio/prelude/AssociativeFlatten.scala
+++ src/main/scala/zio/prelude/AssociativeFlatten.scala
@@ -1,10 +1,14 @@
 package zio.prelude
 
-import scala.annotation.implicitNotFound
-
+import zio._
 import zio.prelude.coherent.AssociativeFlattenCovariantEqualF
+import zio.stream.ZStream
 import zio.test.TestResult
 import zio.test.laws._
+
+import scala.annotation.implicitNotFound
+import scala.concurrent.Future
+import scala.util.Try
 
 /**
  * `AssociativeFlatten` describes a type that can be "flattened" in an
@@ -48,12 +52,48 @@ object AssociativeFlatten extends LawfulF.Covariant[AssociativeFlattenCovariantE
     associativeFlatten
 
   /**
-   * The `AssociativeFlatten` instance for `Option`.
+   * The `AssociativeFlatten` instance for `Cause`.
    */
-  implicit val OptionAssociativeFlatten: AssociativeFlatten[Option] =
-    new AssociativeFlatten[Option] {
-      def flatten[A](ffa: Option[Option[A]]): Option[A] =
-        ffa.flatten
+  implicit val CauseAssociativeFlatten: AssociativeFlatten[Cause] =
+    new AssociativeFlatten[Cause] {
+      override def flatten[A](ffa: Cause[Cause[A]]): Cause[A] = ffa.flatten
+    }
+
+  /**
+   * The `AssociativeFlatten` instance for `Chunk`.
+   */
+  implicit val ChunkAssociativeFlatten: AssociativeFlatten[Chunk] =
+    new AssociativeFlatten[Chunk] {
+      def flatten[A](ffa: Chunk[Chunk[A]]): Chunk[A] = ffa.flatten
+    }
+
+  /**
+   * The `AssociativeFlatten` instance for `Either`.
+   */
+  implicit def EitherAssociativeFlatten[E]: AssociativeFlatten[({ type lambda[+a] = Either[E, a] })#lambda] =
+    new AssociativeFlatten[({ type lambda[+a] = Either[E, a] })#lambda] {
+
+      def flatten[A](ffa: Either[E, Either[E, A]]): Either[E, A] = ffa match {
+        case Left(e)         => Left(e)
+        case Right(Left(e))  => Left(e)
+        case Right(Right(a)) => Right(a)
+      }
+    }
+
+  /**
+   * The `AssociativeFlatten` instance for `Exit`.
+   */
+  implicit def ExitAssociativeFlatten[E]: AssociativeFlatten[({ type lambda[+a] = Exit[E, a] })#lambda] =
+    new AssociativeFlatten[({ type lambda[+a] = Exit[E, a] })#lambda] {
+      def flatten[A](ffa: Exit[E, Exit[E, A]]): Exit[E, A] = ffa.flatten
+    }
+
+  /**
+   * The `AssociativeFlatten` instance for `Future`.
+   */
+  implicit val FutureAssociativeFlatten: AssociativeFlatten[Future] =
+    new AssociativeFlatten[Future] {
+      def flatten[A](ffa: Future[Future[A]]): Future[A] = ffa.flatten
     }
 
   /**
@@ -63,6 +103,71 @@ object AssociativeFlatten extends LawfulF.Covariant[AssociativeFlattenCovariantE
     new AssociativeFlatten[Id] {
       def flatten[A](ffa: Id[Id[A]]): Id[A] = Id.unwrap(ffa)
     }
+
+  /**
+   * The `AssociativeFlatten` instance for `List`.
+   */
+  implicit val ListAssociativeFlatten: AssociativeFlatten[List] =
+    new AssociativeFlatten[List] {
+      def flatten[A](ffa: List[List[A]]): List[A] = ffa.flatten
+    }
+
+  /**
+   * The `AssociativeFlatten` instance for `NonEmptyChunk`.
+   */
+  implicit val NonEmptyChunkAssociativeFlatten: AssociativeFlatten[NonEmptyChunk] =
+    new AssociativeFlatten[NonEmptyChunk] {
+      def flatten[A](ffa: NonEmptyChunk[NonEmptyChunk[A]]): NonEmptyChunk[A] = ffa.flatten
+    }
+
+  /**
+   * The `AssociativeFlatten` instance for `Option`.
+   */
+  implicit val OptionAssociativeFlatten: AssociativeFlatten[Option] =
+    new AssociativeFlatten[Option] {
+      def flatten[A](ffa: Option[Option[A]]): Option[A] = ffa.flatten
+    }
+
+  /**
+   * The `AssociativeFlatten` instance for `Try`.
+   */
+  implicit val TryAssociativeFlatten: AssociativeFlatten[Try] =
+    new AssociativeFlatten[Try] {
+      def flatten[A](ffa: Try[Try[A]]): Try[A] = ffa.flatten
+    }
+
+  /**
+   * The `AssociativeFlatten` instance for `Vector`.
+   */
+  implicit val VectorAssociativeFlatten: AssociativeFlatten[Vector] =
+    new AssociativeFlatten[Vector] {
+      def flatten[A](ffa: Vector[Vector[A]]): Vector[A] = ffa.flatten
+    }
+
+  /**
+   * The `AssociativeFlatten` instance for `ZIO`.
+   */
+  implicit def ZIOAssociativeFlatten[R, E]: AssociativeFlatten[({ type lambda[+a] = ZIO[R, E, a] })#lambda] =
+    new AssociativeFlatten[({ type lambda[+a] = ZIO[R, E, a] })#lambda] {
+      def flatten[A](ffa: ZIO[R, E, ZIO[R, E, A]]): ZIO[R, E, A] = ffa.flatten
+    }
+
+  /**
+   * The `AssociativeFlatten` instance for `ZManaged`.
+   */
+  implicit def ZManagedAssociativeFlatten[R, E]: AssociativeFlatten[({ type lambda[+a] = ZManaged[R, E, a] })#lambda] =
+    new AssociativeFlatten[({ type lambda[+a] = ZManaged[R, E, a] })#lambda] {
+      def flatten[A](ffa: ZManaged[R, E, ZManaged[R, E, A]]): ZManaged[R, E, A] = ffa.flatten
+    }
+
+  /**
+   * The `AssociativeFlatten` instance for `ZStream`.
+   */
+  implicit def ZStreamAssociativeFlatten[R, E]: AssociativeFlatten[({ type lambda[+a] = ZStream[R, E, a] })#lambda] =
+    new AssociativeFlatten[({ type lambda[+a] = ZStream[R, E, a] })#lambda] {
+      def flatten[A](ffa: ZStream[R, E, ZStream[R, E, A]]): ZStream[R, E, A] = ffa.flatten
+    }
+
 }
 
 trait AssociativeFlattenSyntax {
