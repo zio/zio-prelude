1d9f111c0533977a367b36c5e54583f40fc42366
Jorge Aliss <jaliss@gmail.com>
2020-07-29 09:17:30 -0300

    Implement AssociativeFlatten Instance Map (#129)

--- src/test/scala/zio/prelude/AssociativeFlattenSpec.scala
+++ src/test/scala/zio/prelude/AssociativeFlattenSpec.scala
@@ -1,6 +1,7 @@
 package zio.prelude
 
 import zio.prelude.IdentityFlattenSpec.genFEither
+import zio.prelude.CovariantSpec.genFMap
 import zio.test._
 import zio.test.laws._
 
@@ -12,7 +13,8 @@ object AssociativeFlattenSpec extends DefaultRunnableSpec {
       testM("either")(checkAllLaws(AssociativeFlatten)(genFEither, Gen.anyInt)),
       testM("option")(checkAllLaws(AssociativeFlatten)(GenF.option, Gen.anyInt)),
       testM("list")(checkAllLaws(AssociativeFlatten)(GenF.list, Gen.anyString)),
-      testM("vector")(checkAllLaws(AssociativeFlatten)(GenF.vector, Gen.anyString))
+      testM("vector")(checkAllLaws(AssociativeFlatten)(GenF.vector, Gen.anyString)),
+      testM("map")(checkAllLaws(AssociativeFlatten)(genFMap, Gen.anyInt))
     )
   )
 }
