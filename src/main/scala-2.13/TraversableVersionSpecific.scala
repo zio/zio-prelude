5e0821dc89a13bfbebc0b7f9d89a3a3169a85cd3
Adam Fraser <adam.fraser@gmail.com>
2020-07-29 12:06:50 -0700

    add Traversable instances (#166)

---
+++ src/main/scala-2.13/TraversableVersionSpecific.scala
@@ -0,0 +1,27 @@
+package zio.prelude
+
+import scala.collection.BuildFrom
+
+trait TraversableVersionSpecific {
+
+  /**
+   * Derives a `Traverable[F]` from an `Iterable[F]`.
+   */
+  implicit def IterableTraversable[F[+a] <: Iterable[a]](implicit derive: DeriveBuildFrom[F]): Traversable[F] =
+    new Traversable[F] {
+      def foreach[G[+_]: IdentityBoth: Covariant, A, B](fa: F[A])(f: A => G[B]): G[F[B]] =
+        fa.foldLeft(derive.derive[B].newBuilder(fa).succeed)((bs, a) => bs.zipWith(f(a))(_ += _)).map(_.result())
+    }
+
+  trait DeriveBuildFrom[F[+_]] {
+    def derive[A]: BuildFrom[F[Any], A, F[A]]
+  }
+
+  object DeriveBuildFrom {
+    implicit def default[F[+_]](implicit bf: BuildFrom[F[Any], Any, F[Any]]): DeriveBuildFrom[F] =
+      new DeriveBuildFrom[F] {
+        def derive[A]: BuildFrom[F[Any], A, F[A]] =
+          bf.asInstanceOf[BuildFrom[F[Any], A, F[A]]]
+      }
+  }
+}
