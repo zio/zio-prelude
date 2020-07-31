223b5f13ccbcaa9ea1cb804e7b4d55adeed86423
Adam Fraser <adam.fraser@gmail.com>
2020-07-31 10:51:10 -0700

    implement CommutativeEither instance for Future (#174)

--- src/test/scala/zio/prelude/CommutativeEitherSpec.scala
+++ src/test/scala/zio/prelude/CommutativeEitherSpec.scala
@@ -1,11 +1,22 @@
 package zio.prelude
 
+import scala.concurrent.Future
+
+import zio.ZIO
 import zio.test._
 
 object CommutativeEitherSpec extends DefaultRunnableSpec {
 
   def spec = suite("CommutativeEitherSpec")(
-    suite("laws")(
-      )
+    testM("FutureCommutativeEither returns the first future that is completed") {
+      for {
+        l <- ZIO.fromFuture { implicit ec =>
+              Future.unit <|> Future.never
+            }
+        r <- ZIO.fromFuture { implicit ec =>
+              Future.never <|> Future.unit
+            }
+      } yield assert(l.swap)(equalTo(r))
+    }
   )
 }
