627113f8021d59a382b9c99469415820f05797ff
Dejan Mijic <dmijic@acm.org>
2020-08-04 12:47:36 +0200

    Fix scoping

--- src/main/scala/zio/prelude/Equal.scala
+++ src/main/scala/zio/prelude/Equal.scala
@@ -258,7 +258,7 @@ object Equal extends Lawful[Equal] {
    * return `true`, which is different from the behavior of `Double#equals`.
    */
   implicit val DoubleEqual: Equal[Double] =
-    make((n1, n2) => Double.doubleToRawLongBits(n1) == Double.doubleToRawLongBits(n2))
+    make((n1, n2) => java.lang.Double.doubleToRawLongBits(n1) == java.lang.Double.doubleToRawLongBits(n2))
 
   /**
    * Derives an `Equal[Either[A, B]]` given an `Equal[A]` and an `Equal[B]`.
@@ -272,7 +272,7 @@ object Equal extends Lawful[Equal] {
    * return `true`, which is different from the behavior of `Float#equals`.
    */
   implicit val FloatEqual: Equal[Float] =
-    make((n1, n2) => Float.floatToRawIntBits(n1) == Float.floatToRawIntBits(n2))
+    make((n1, n2) => java.lang.Float.floatToRawIntBits(n1) == java.lang.Float.floatToRawIntBits(n2))
 
   /**
    * Equality for `Int` values.
