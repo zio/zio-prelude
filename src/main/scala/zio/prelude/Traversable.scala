61481ece4fa3d05fb6c2ee8a5c244cf70ce042a2
Michael Thomas <Michaelt293@users.noreply.github.com>
2020-08-09 07:43:51 +1000

    Added contains method to Traversable (#194)
    * Added contains method to Traversable
    
    * Added type bound

--- src/main/scala/zio/prelude/Traversable.scala
+++ src/main/scala/zio/prelude/Traversable.scala
@@ -25,6 +25,12 @@ trait Traversable[F[+_]] extends Covariant[F] {
   def foreach[G[+_]: IdentityBoth: Covariant, A, B](fa: F[A])(f: A => G[B]): G[F[B]]
 
   /**
+   * Returns whether the collection contains the specified element.
+   */
+  def contains[A, A1 >: A](fa: F[A])(a: A1)(implicit A: Equal[A1]): Boolean =
+    exists(fa)(_ === a)
+
+  /**
    * Returns the number of elements in the collection that satisfy the
    * specified prediate.
    */
@@ -300,6 +306,8 @@ trait TraversableSyntax {
   implicit class TraversableOps[F[+_], A](private val self: F[A]) {
     def foreach[G[+_]: IdentityBoth: Covariant, B](f: A => G[B])(implicit F: Traversable[F]): G[F[B]] =
       F.foreach(self)(f)
+    def contains[A1 >: A](a: A1)(implicit A: Equal[A1], F: Traversable[F]): Boolean =
+      F.contains[A, A1](self)(a)
     def count(f: A => Boolean)(implicit F: Traversable[F]): Int =
       F.count(self)(f)
     def exists(f: A => Boolean)(implicit F: Traversable[F]): Boolean =
