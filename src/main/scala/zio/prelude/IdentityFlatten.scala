289faded13e2e04b589766cb1a01ae956effacbc
Dave Nicponski <dave.nicponski@gmail.com>
2020-08-10 06:27:31 -0400

    Fix typo in IdentityFlatten (#198)

--- src/main/scala/zio/prelude/IdentityFlatten.scala
+++ src/main/scala/zio/prelude/IdentityFlatten.scala
@@ -13,7 +13,7 @@ import scala.util.{ Success, Try }
 /**
  * `IdentityFlatten` described a type that can be "flattened" in an
  * associative way and has an identity element with respect to that operation.
- * For example, with a list we can always vacuosly add a layer by wrapping a
+ * For example, with a list we can always vacuously add a layer by wrapping a
  * list in another list constructor and flattening the resulting list always
  * returns the original list unchanged.
  */
