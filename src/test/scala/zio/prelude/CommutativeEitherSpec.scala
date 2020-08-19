ebeb668b600a302c0719d37ce98fe753a965f4f9
Ondra Pelech <ondra.pelech@gmail.com>
2020-08-19 11:49:45 +0200

    don't sleep infinite

--- src/test/scala/zio/prelude/CommutativeEitherSpec.scala
+++ src/test/scala/zio/prelude/CommutativeEitherSpec.scala
@@ -10,24 +10,10 @@ object CommutativeEitherSpec extends DefaultRunnableSpec {
     testM("FutureCommutativeEither returns the first future that is completed") {
       for {
         l <- ZIO.fromFuture { implicit ec =>
-              Future.successful(()) <|> Future {
-                blocking {
-                  while (true) {
-                    Thread.sleep(5000)
-                    println("infinite future 1")
-                  }
-                }
-              }
+              Future.successful(()) <|> Future { blocking(Thread.sleep(60 * 1000)) }
             }
         r <- ZIO.fromFuture { implicit ec =>
-              Future {
-                blocking {
-                  while (true) {
-                    Thread.sleep(5000)
-                    println("infinite future 2")
-                  }
-                }
-              } <|> Future.successful(())
+              Future { blocking(Thread.sleep(60 * 1000)) } <|> Future.successful(())
             }
       } yield assert(l.merge)(equalTo(r.merge))
     }
