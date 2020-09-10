998bb48b614d4f696fab041394bdda49386a2026
Ondra Pelech <ondra.pelech@gmail.com>
2020-09-10 23:11:01 +0200

    Add `Function1` `Divariant` instance (#268)

--- src/main/scala/zio/prelude/Divariant.scala
+++ src/main/scala/zio/prelude/Divariant.scala
@@ -18,6 +18,16 @@ trait Divariant[:=>[-_, +_]] { self =>
 
   def rightMap[A, B, C](f: B => C): (A :=> B) => (A :=> C)
 }
-object Divariant             {
+
+object Divariant {
   final case class Join[:=>[-_, +_], A](value: A :=> A)
+
+  implicit val Function1Divariant: Divariant[Function1[-*, +*]] = new Divariant[Function1[-*, +*]] {
+    override def leftMap[A, B, C](c2a: C => A): (A => B) => C => B = { a2b => c =>
+      c |> c2a |> a2b
+    }
+    override def rightMap[A, B, C](b2c: B => C): (A => B) => A => C = { a2b => a =>
+      a |> a2b |> b2c
+    }
+  }
 }
