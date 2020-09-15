48154a5a1016549c72866e492709b174bd883256
Piotr Paradziński <piotr.paradzinski@scalac.io>
2020-09-15 16:00:21 +0200

    bicontravariant sbt fix

--- src/main/scala/zio/prelude/Bicovariant.scala
+++ src/main/scala/zio/prelude/Bicovariant.scala
@@ -1,6 +1,6 @@
 package zio.prelude
 
-trait RightCovariant[:=>[_,+_]] {
+trait RightCovariant[:=>[_, +_]] {
   def deriveCovariant[A]: Covariant[({ type lambda[+B] = A :=> B })#lambda] =
     new Covariant[({ type lambda[+B] = A :=> B })#lambda] {
       def map[B, C](f: B => C): A :=> B => A :=> C = rightMap(f)
@@ -11,11 +11,11 @@ trait RightCovariant[:=>[_,+_]] {
 
 trait Bicovariant[:=>[+_, +_]] extends RightCovariant[:=>] {
 
-  def bimap[A,B,AA,BB](f: A => AA, g: B => BB): (A :=> B) => (AA :=> BB)
+  def bimap[A, B, AA, BB](f: A => AA, g: B => BB): (A :=> B) => (AA :=> BB)
 
-  def leftMap[A,B,AA](f: A => AA): (A :=> B) => (AA :=> B) =
+  def leftMap[A, B, AA](f: A => AA): (A :=> B) => (AA :=> B)  =
     bimap(f, identity[B])
-  def rightMap[A,B,BB](g: B => BB): (A :=> B) => (A :=> BB) =
+  def rightMap[A, B, BB](g: B => BB): (A :=> B) => (A :=> BB) =
     bimap(identity[A], g)
 }
 
@@ -23,15 +23,14 @@ object Bicovariant {
 
   implicit val Tuple2Bicovariant: Bicovariant[Tuple2] =
     new Bicovariant[Tuple2] {
-      override def bimap[A,B,AA,BB](f: A => AA, g: B => BB): ((A, B)) => (AA, BB) =
-        { case(a,b) => (f(a), g(b)) }
+      override def bimap[A, B, AA, BB](f: A => AA, g: B => BB): ((A, B)) => (AA, BB) = { case (a, b) => (f(a), g(b)) }
     }
 
   implicit val EitherBicovariant: Bicovariant[Either] =
     new Bicovariant[Either] {
-      override def bimap[A,B,AA,BB](f: A => AA, g: B => BB): Either[A, B] => Either[AA, BB] = {
+      override def bimap[A, B, AA, BB](f: A => AA, g: B => BB): Either[A, B] => Either[AA, BB] = {
         case Right(a) => Right(g(a))
-        case Left(b) => Left(f(b))
+        case Left(b)  => Left(f(b))
       }
     }
 }
