bdd13d569c8266363b324f8e758ca86c7708d648
Ondra Pelech <ondra.pelech@gmail.com>
2020-11-06 00:48:00 +0100

    fix

--- src/test/scala/zio/prelude/AssociativeSpec.scala
+++ src/test/scala/zio/prelude/AssociativeSpec.scala
@@ -4,8 +4,11 @@ import zio.prelude.newtypes.{ And, Or, Prod, Sum }
 import zio.test.laws._
 import zio.test.{ testM, _ }
 
+import com.github.ghik.silencer.silent
+
 object AssociativeSpec extends DefaultRunnableSpec {
 
+  @silent("Unused import")
   def spec: ZSpec[Environment, Failure] =
     suite("AssociativeSpec")(
       suite("laws")(
