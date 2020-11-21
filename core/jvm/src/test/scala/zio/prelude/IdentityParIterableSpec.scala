c22afb9fbe4d6567424d8aa31c44b7171cd64058
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2020-11-21 20:40:55 +0100

    Build on scalajs

---
+++ core/jvm/src/test/scala/zio/prelude/IdentityParIterableSpec.scala
@@ -0,0 +1,29 @@
+package zio.prelude
+
+import zio.prelude.newtypes.Sum
+import zio.test._
+import zio.blocking.Blocking
+
+object IdentityParIterableSpec extends DefaultRunnableSpec {
+  private val ParallelCollectionCompatibility = {
+    object Compat {
+      object CollectionConverters
+    }
+    import Compat._
+    {
+      import scala.collection.parallel._
+      CollectionConverters
+    }
+  }
+  import ParallelCollectionCompatibility._
+
+  val spec: Spec[Blocking, TestFailure[Throwable], TestSuccess] =
+    suite("ParIterable")(
+      test("ParIterable non-empty returns a value") {
+        assert(List(Sum(1), Sum(2), Sum(3), Sum(4)).par.reduceIdentity)(equalTo(Sum(10)))
+      },
+      testM("ParIterable empty returns the `identity` element, non-blocking") {
+        assertM(List[Sum[Int]]().par.reduceIdentityM)(equalTo(Sum(0)))
+      }
+    )
+}
