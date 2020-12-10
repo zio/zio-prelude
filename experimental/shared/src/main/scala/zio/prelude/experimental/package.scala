1e9fc8f4b9c37100a348b41bcc7e5f61ec1bcc27
Ondra Pelech <ondra.pelech@gmail.com>
2020-12-10 01:24:13 +0100

    Add BooleanAlgebra

--- experimental/shared/src/main/scala/zio/prelude/experimental/package.scala
+++ experimental/shared/src/main/scala/zio/prelude/experimental/package.scala
@@ -6,13 +6,17 @@ package object experimental extends ComplementShapeSyntax with JoinMeetSyntax {
 
     import zio.prelude.classic._
 
-    type Lattice[A]                  = Absorption.Aux[A, Semilattice, Semilattice]
-    type BoundedLattice[A]           = Absorption.Aux[A, BoundedSemilattice, BoundedSemilattice]
-    type OrthoComplementedLattice[A] = BoundedLattice[A]
-      with Complement.Aux[A, BoundedSemilattice, BoundedSemilattice]
-      with Involution.Aux[A, BoundedSemilattice, BoundedSemilattice]
-    type DistributiveLattice[A]      = Lattice[A] with DistributiveJoinMeet.Aux[A, Semilattice, Semilattice]
-
+    type Lattice[A]                  = JoinMeetShape.Aux[A, Semilattice, Semilattice]
+    type BoundedLattice[A]           = JoinMeetShape.Aux[A, BoundedSemilattice, BoundedSemilattice]
+    type OrthoComplementedLattice[A] = Complement[A] with Involution[A] {
+      type Join[x] = BoundedSemilattice[x]
+      type Meet[x] = BoundedSemilattice[x]
+    }
+    type DistributiveLattice[A]      = DistributiveJoinMeet.Aux[A, Semilattice, Semilattice]
+    type BooleanAlgebra[A]           = Absorption[A] with Complement[A] with DistributiveJoinMeet[A] {
+      type Join[x] = BoundedSemilattice[x]
+      type Meet[x] = BoundedSemilattice[x]
+    }
   }
 
 }
