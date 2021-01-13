e3eac1782972a892db223d7d60063feaf4c22e48
Ondra Pelech <ondra.pelech@gmail.com>
2021-01-13 23:25:37 +0100

    Silencer: remove unused silent (#495)

--- core/shared/src/test/scala/zio/prelude/AssociativeSpec.scala
+++ core/shared/src/test/scala/zio/prelude/AssociativeSpec.scala
@@ -1,11 +1,9 @@
 package zio.prelude
 
-import com.github.ghik.silencer.silent
 import zio.test._
 
 object AssociativeSpec extends DefaultRunnableSpec {
 
-  @silent("Unused import")
   def spec: ZSpec[Environment, Failure] =
     suite("AssociativeSpec")(
       suite("laws")(
