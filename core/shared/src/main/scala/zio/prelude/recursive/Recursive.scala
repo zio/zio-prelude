9a82d3d8f059c4286f0c73b35dd5f5e1a8c231f4
Adam Fraser <adam.fraser@gmail.com>
2021-01-29 22:23:27 -0800

    implement foldRecursive and unfoldRecursive

--- core/shared/src/main/scala/zio/prelude/recursive/Recursive.scala
+++ core/shared/src/main/scala/zio/prelude/recursive/Recursive.scala
@@ -11,10 +11,23 @@ final case class Recursive[Case[+_]](caseValue: Case[Recursive[Case]]) {
     foreach: ForEach[Case]
   ): F[Z] =
     fold[F[Z]](_.flip.flatMap(f))
+
+  def foldRecursive[Z](f: Case[(Recursive[Case], Z)] => Z)(implicit covariant: Covariant[Case]): Z =
+    f(caseValue.map(recursive => recursive -> recursive.foldRecursive(f)))
 }
 
 object Recursive {
 
   def unfold[Case[+_], Z](z: Z)(f: Z => Case[Z])(implicit covariant: Covariant[Case]): Recursive[Case] =
     Recursive(f(z).map(unfold(_)(f)))
+
+  def unfoldRecursive[Case[+_], Z](z: Z)(
+    f: Either[Recursive[Case], Z] => Case[Either[Recursive[Case], Z]]
+  )(implicit covariant: Covariant[Case]): Recursive[Case] =
+    Recursive {
+      f(Right(z)).map {
+        case Left(recursive) => recursive
+        case Right(z)        => unfoldRecursive(z)(f)
+      }
+    }
 }
