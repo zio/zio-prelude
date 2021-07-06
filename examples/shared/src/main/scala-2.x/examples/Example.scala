10dd802b0b32227cf4420780735d10f5fb2a6526
Kit Langton <kit.langton@gmail.com>
2021-07-06 08:50:25 -0700

    formatting

--- examples/shared/src/main/scala-2.x/examples/Example.scala
+++ examples/shared/src/main/scala-2.x/examples/Example.scala
@@ -2,11 +2,12 @@ package examples
 
 import zio.prelude.macros.Refined
 import zio.prelude.refined.Assertion._
+
 import Regex._
 
 object Example extends App {
-  val Between10And20 = Refined[Int]((greaterThan(10) && lessThan(20)) || equalTo(30))
-  object Amount extends Between10And20.Newtype {
+  private val Between10And20 = Refined[Int]((greaterThan(10) && lessThan(20)) || equalTo(30))
+  private object Amount extends Between10And20.Newtype {
     type Amount = Amount.Type
     implicit final class AmountOps(private val self: Amount) extends AnyVal {
       def ten = 10
@@ -17,24 +18,21 @@ object Example extends App {
 
   val amt: Amount = Amount(30)
 
-  val JorgeRefined = Refined[String](matches(start ~ literal("Jorge") ~ end))
-  object Jorge extends JorgeRefined.Newtype {
+  private val JorgeRefined = Refined[String](matches(start ~ literal("Jorge") ~ end))
+  private object Jorge extends JorgeRefined.Newtype {
     type Jorge = Jorge.Type
   }
   import Jorge._
   val jorge: Jorge = Jorge("Jorge")
 
-  val RegexRefined = Refined[String] {
+  private val RegexRefined = Refined[String] {
     matches {
       anyChar ~ alphanumeric ~ (nonAlphanumeric | whitespace) ~ nonWhitespace ~ digit.min(0) ~ nonDigit.min(1) ~
         "hello" ~ anyOf('a', 'b', 'c').min(2) ~ notAnyOf('d', 'e', 'f').min(0).max(1) ~
         inRange('a', 'z').max(2) ~ notInRange('1', '5').min(1).max(3)
     }
   }
-  object MyRegex extends RegexRefined.Newtype {
+  private object MyRegex extends RegexRefined.Newtype {
     type MyRegex = MyRegex.Type
   }
-
-//  import MyRegex._
-//  val myRegex: MyRegex = MyRegex("a")
 }
