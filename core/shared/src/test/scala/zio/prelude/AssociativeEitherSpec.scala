a926863a57bcb393b35f6b3d3c3480a1bb3629ea
Adam Fraser <adam.fraser@gmail.com>
2021-07-25 09:50:47 -0700

    format

--- core/shared/src/test/scala/zio/prelude/AssociativeEitherSpec.scala
+++ core/shared/src/test/scala/zio/prelude/AssociativeEitherSpec.scala
@@ -8,7 +8,7 @@ object AssociativeEitherSpec extends DefaultRunnableSpec {
   def spec: ZSpec[Environment, Failure] =
     suite("AssociativeEitherSpec")(
       suite("laws")(
-        testM("either")(checkAllLaws(AssociativeEither)(GenF.either(Gen.anyInt), Gen.anyInt)),
+        testM("either")(checkAllLaws(AssociativeEither)(GenF.either(Gen.anyInt), Gen.anyInt))
       )
     )
 }
