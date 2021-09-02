5d17a314dba1b04b95ad87198652911bafc48d46
Kit Langton <kit.langton@gmail.com>
2021-09-01 20:02:55 -0700

    remove wrap

--- core/shared/src/main/scala-2.x/zio/prelude/NewtypeVersionSpecific.scala
+++ core/shared/src/main/scala-2.x/zio/prelude/NewtypeVersionSpecific.scala
@@ -4,19 +4,15 @@ import zio.prelude.refined.{QuotedRefinement, Refinement}
 
 trait NewtypeVersionSpecific[A] { self: NewtypeModule#Newtype[A] =>
 
+  val hi = 1
+
   /**
    * Converts an instance of the underlying type to an instance of the
    * newtype.
    */
-  def apply(value: A): Type = macro zio.prelude.refined.Macros.wrap_impl[A, Type]
+  def apply(value: A): Type = macro zio.prelude.refined.Macros.apply_impl[A, Type]
 
   def refine(refinement: Refinement[A]): QuotedRefinement[A] = macro zio.prelude.refined.Macros.refine_impl[A]
-
-  /**
-   * Converts an instance of the underlying type to an instance of the
-   * newtype.
-   */
-  def wrap(value: A): Type = macro zio.prelude.refined.Macros.wrap_impl[A, Type]
 
   /**
    * Converts an instance of a type parameterized on the underlying type
