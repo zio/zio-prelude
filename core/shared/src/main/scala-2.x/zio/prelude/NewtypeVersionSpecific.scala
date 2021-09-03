ececa9b5f21487fd0207b6d9baa9b42b434534bb
Kit Langton <kit.langton@gmail.com>
2021-09-03 16:37:16 -0700

    add doc string to refine

--- core/shared/src/main/scala-2.x/zio/prelude/NewtypeVersionSpecific.scala
+++ core/shared/src/main/scala-2.x/zio/prelude/NewtypeVersionSpecific.scala
@@ -29,15 +29,54 @@ trait NewtypeCompanionVersionSpecific {
 trait NewtypeVersionSpecific[A] { self: NewtypeModule#Newtype[A] =>
 
   /**
-   * Converts an instance of the underlying type to an instance of the
-   * newtype.
+   * Converts an instance of the underlying type to an instance of the newtype.
+   *
+   * If there is a `def refinement` (see [[refine]]), the value will be checked
+   * at compile-time.
    */
   def apply(value: A): Type = macro zio.prelude.Macros.wrap_impl[A, Type]
 
+  /**
+   * Converts multiple instances of the underlying type to [[NonEmptyChunk]] of
+   * instances of the newtype.
+   *
+   * If there is a `def refinement` (see [[refine]]), each value will be checked
+   * at compile-time.
+   */
   def apply(value: A, values: A*): NonEmptyChunk[Type] = macro zio.prelude.Macros.applyMany_impl[A, Type]
 
   def make(value: A): Validation[String, Type] = macro zio.prelude.Macros.make_impl[A, Type]
 
+  /**
+   * This method is used to generate Newtype that can be validated at
+   * compile-time. This must wrap a [[Refinement]] and be assigned to
+   * `def refinement`.
+   *
+   * For example, here is a refined Newtype for Natural numbers. Natural
+   * numbers are whole numbers greater than or equal to 0.
+   *
+   * {{{
+   * import zio.prelude.Subtype
+   * import zio.prelude.Refinement._
+   *
+   * type Natural = Natural.Type
+   * object Natural extends Subtype[Int] {
+   *   def refinement = refine(greaterThanOrEqualTo(0))
+   * }
+   * }}}
+   *
+   * With this `refinement` defined, `Natural.apply` will check literal values
+   * at compile-time, failing with an error message if the Refinement is not
+   * satisfied.
+   *
+   * `Natural(-10)` would render "`-10 failed to satisfy greaterThanOrEqualTo(10)`"
+   *
+   * IMPORTANT: Due to the macro machinery powering this feature, you must be
+   * sure to NOT ANNOTATE `def refinement` with a type (`QuotedRefinement`). If
+   * you do so, the macro will not be able to run the provided refinement at
+   * compile-time and will fail with a message containing this very same
+   * information.
+   */
   def refine(refinement: Refinement[A]): QuotedRefinement[A] = macro zio.prelude.Macros.refine_impl[A]
 
   /**
