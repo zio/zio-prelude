57251b115b4c0bfb4ec58d64ee0dbb0ef1ef3a8a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-04 04:09:07 +0200

    Update kind-projector to 0.13.2 (#720)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -237,7 +237,7 @@ object BuildHelper {
         Seq(
           "com.github.ghik" % "silencer-lib"            % SilencerVersion % Provided cross CrossVersion.full,
           compilerPlugin("com.github.ghik" % "silencer-plugin" % SilencerVersion cross CrossVersion.full),
-          compilerPlugin("org.typelevel"  %% "kind-projector"  % "0.13.1" cross CrossVersion.full)
+          compilerPlugin("org.typelevel"  %% "kind-projector"  % "0.13.2" cross CrossVersion.full)
         )
     },
     semanticdbEnabled := scalaVersion.value != ScalaDotty, // enable SemanticDB
