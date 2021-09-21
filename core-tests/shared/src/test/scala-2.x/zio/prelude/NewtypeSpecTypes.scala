9df5408ff3c5e352fdc597b676368f9fd9d5fd89
Ondra Pelech <ondra.pelech@gmail.com>
2021-09-21 23:07:39 +0200

    Fix wrong rename, should be Regex.anyChar

--- core-tests/shared/src/test/scala-2.x/zio/prelude/NewtypeSpecTypes.scala
+++ core-tests/shared/src/test/scala-2.x/zio/prelude/NewtypeSpecTypes.scala
@@ -86,7 +86,7 @@ object NewtypeSpecTypes {
   object Password extends Newtype[String] {
 
     def assertion =
-      assert(Assertion.matches(literal("a").+ ~ char.between(1, 3) ~ literal("oh").*))
+      assert(Assertion.matches(literal("a").+ ~ anyChar.between(1, 3) ~ literal("oh").*))
   }
 
   // These should compile
