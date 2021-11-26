81440041a271702f730d88b4405b2cad6bc5aab8
Yisrael Union <yisraelU@users.noreply.github.com>
2021-11-25 20:32:27 -0500

    add fromOptions constructor for the These data type (#811)
    Co-authored-by: Ondra Pelech <ondra.pelech@gmail.com>

--- core/shared/src/main/scala/zio/prelude/These.scala
+++ core/shared/src/main/scala/zio/prelude/These.scala
@@ -90,4 +90,14 @@ object These {
       override def combine(l: => These[A, B], r: => These[A, B]): These[A, B] = l.combine(r)
     }
 
+  /**
+   *  constructor from two options to an option of These.
+   */
+  def fromOptions[A, B](opA: Option[A], opB: Option[B]): Option[These[A, B]] =
+    (opA, opB) match {
+      case (Some(a), Some(b)) => Some(Both(a, b))
+      case (Some(a), _)       => Some(Left(a))
+      case (_, Some(b))       => Some(Right(b))
+      case _                  => None
+    }
 }
