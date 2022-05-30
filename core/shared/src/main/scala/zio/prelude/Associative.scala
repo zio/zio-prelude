6f0e916baf2a06020d024b10bc68fa2e7a4fa660
Alex Simkin <alex.simkin@outlook.com>
2022-05-30 18:03:28 +1000

    #919: ZValidation associative error aggregation. (#920)
    * #919: ZValidation associative error aggregation.
    
    * #919: Cause is commutative.
    
    * #919: newtypes for Cause.

--- core/shared/src/main/scala/zio/prelude/Associative.scala
+++ core/shared/src/main/scala/zio/prelude/Associative.scala
@@ -17,8 +17,8 @@
 package zio.prelude
 
 import zio.duration.{Duration => ZIODuration}
-import zio.prelude.newtypes.{And, AndF, First, Last, Max, Min, Natural, Or, OrF, Prod, Sum}
-import zio.{Chunk, NonEmptyChunk}
+import zio.prelude.newtypes.{And, AndF, AndThen, Both, First, Last, Max, Min, Natural, Or, OrF, Prod, Sum}
+import zio.{Cause, Chunk, NonEmptyChunk}
 
 import scala.annotation.tailrec
 
@@ -309,6 +309,32 @@ object Associative extends AssociativeLowPriority {
       def combine(l: => Sum[Char], r: => Sum[Char]): Sum[Char] = Sum((l + r).toChar)
       val identity: Sum[Char]                                  = Sum(0)
       def inverse(l: => Sum[Char], r: => Sum[Char]): Sum[Char] = Sum((l - r).toChar)
+    }
+
+  /**
+   * The `Commutative` and `Identity` instance for `Cause`.
+   */
+  implicit def CauseCommutativeIdentity[A]: Commutative[Both[Cause[A]]] with Identity[Both[Cause[A]]] =
+    new Commutative[Both[Cause[A]]] with Identity[Both[Cause[A]]] {
+      def combine(l: => Both[Cause[A]], r: => Both[Cause[A]]): Both[Cause[A]] = {
+        val lUnwrapped: Cause[A] = l
+        val rUnwrapped: Cause[A] = r
+        Both(lUnwrapped && rUnwrapped)
+      }
+      val identity: Both[Cause[A]]                                            = Both(Cause.empty)
+    }
+
+  /**
+   * The `Associative` and `Identity` instance for `Cause`.
+   */
+  implicit def CauseAssociativeIdentity[A]: Associative[AndThen[Cause[A]]] with Identity[AndThen[Cause[A]]] =
+    new Associative[AndThen[Cause[A]]] with Identity[AndThen[Cause[A]]] {
+      def combine(l: => AndThen[Cause[A]], r: => AndThen[Cause[A]]): AndThen[Cause[A]] = {
+        val lUnwrapped: Cause[A] = l
+        val rUnwrapped: Cause[A] = r
+        AndThen(lUnwrapped ++ rUnwrapped)
+      }
+      val identity: AndThen[Cause[A]]                                                  = AndThen(Cause.empty)
     }
 
   /**
