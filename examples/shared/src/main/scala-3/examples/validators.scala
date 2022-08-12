01b131ce04f695627b4d24d218ad1c3dc271c3ff
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-12 23:06:42 +0200

    Update scalafmt-core to 3.5.9 (#952)
    * Update scalafmt-core to 3.5.9
    
    * Reformat with scalafmt 3.5.9
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.5.9' to .git-blame-ignore-revs

--- examples/shared/src/main/scala-3/examples/validators.scala
+++ examples/shared/src/main/scala-3/examples/validators.scala
@@ -2,6 +2,7 @@ package examples
 
 import zio.prelude._
 
-object PalindromeValidator extends Validator[String](str =>
-  if (str.reverse == str) Right(()) else Left(AssertionError.Failure("isPalindrome"))
-)
+object PalindromeValidator
+    extends Validator[String](str =>
+      if (str.reverse == str) Right(()) else Left(AssertionError.Failure("isPalindrome"))
+    )
