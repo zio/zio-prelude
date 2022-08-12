ffccac39269d362ae0cc5aa161c3721a0c7afea4
Scala Steward <scala_steward@virtuslab.com>
2022-08-12 20:42:45 +0000

    Reformat with scalafmt 3.5.9
    Executed command: scalafmt --non-interactive

--- core-tests/shared/src/test/scala-3/zio/prelude/validators.scala
+++ core-tests/shared/src/test/scala-3/zio/prelude/validators.scala
@@ -2,7 +2,7 @@ package zio.prelude
 
 import zio.prelude._
 
-object PalindromeValidator extends Validator[String](str =>
-  if (str.reverse == str) Right(()) else Left(AssertionError.Failure("isPalindrome"))
-)
-
+object PalindromeValidator
+    extends Validator[String](str =>
+      if (str.reverse == str) Right(()) else Left(AssertionError.Failure("isPalindrome"))
+    )
