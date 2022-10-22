ade423950bef79888727ccc1bbfef9999268c9e6
Scala Steward <scala_steward@virtuslab.com>
2022-10-22 03:17:48 +0000

    Update scala-compiler, scala-library, ... to 2.13.10

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -10,7 +10,7 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 object BuildHelper {
   val Scala211: String = "2.11.12"
   val Scala212: String = "2.12.16"
-  val Scala213: String = "2.13.8"
+  val Scala213: String = "2.13.10"
   val Scala3: String   = "3.2.0"
 
   val SilencerVersion = "1.7.11"
