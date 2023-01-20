5150311848462da57320a81508ff7548b7e606e9
Scala Steward <scala_steward@virtuslab.com>
2023-01-20 02:03:36 +0000

    Reformat with scalafmt 3.7.0
    Executed command: scalafmt --non-interactive

--- core/shared/src/main/scala/zio/prelude/AssociativeBoth.scala
+++ core/shared/src/main/scala/zio/prelude/AssociativeBoth.scala
@@ -428,9 +428,31 @@ object AssociativeBoth extends AssociativeBothLowPriority {
   /**
    * Combines 19 `F` values using the provided function `f`.
    */
-  def mapN[F[
-    +_
-  ]: AssociativeBoth: Covariant, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, B](
+  def mapN[
+    F[
+      +_
+    ]: AssociativeBoth: Covariant,
+    A0,
+    A1,
+    A2,
+    A3,
+    A4,
+    A5,
+    A6,
+    A7,
+    A8,
+    A9,
+    A10,
+    A11,
+    A12,
+    A13,
+    A14,
+    A15,
+    A16,
+    A17,
+    A18,
+    B
+  ](
     a0: F[A0],
     a1: F[A1],
     a2: F[A2],
