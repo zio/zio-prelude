060a078f99638a8a18a81ef29b6a915bc567bf16
Adam Fraser <adam.fraser@gmail.com>
2023-02-14 07:39:17 -0800

    format

--- core/shared/src/main/scala-3/zio/prelude/EqualVersionSpecific.scala
+++ core/shared/src/main/scala-3/zio/prelude/EqualVersionSpecific.scala
@@ -21,35 +21,35 @@ import scala.compiletime.{erasedValue, summonInline}
 
 trait EqualVersionSpecific {
 
-	inline given derived[A](using mirror: Mirror.Of[A]): Equal[A] =
-		val instances = summonAll[mirror.MirroredElemTypes]
-		inline mirror match
-			case sum: Mirror.SumOf[A] => equalSum(sum, instances)
-			case product: Mirror.ProductOf[A] => equalProduct(product, instances)
+  inline given derived[A](using mirror: Mirror.Of[A]): Equal[A] =
+    val instances = summonAll[mirror.MirroredElemTypes]
+    inline mirror match
+      case sum: Mirror.SumOf[A] => equalSum(sum, instances)
+      case product: Mirror.ProductOf[A] => equalProduct(product, instances)
 
-	private inline def summonAll[A <: Tuple]: List[Equal[_]] =
-		inline erasedValue[A] match {
-			case _: EmptyTuple => Nil
-			case _: (t *: ts)  => summonInline[Equal[t]] :: summonAll[ts]
-		}
+  private inline def summonAll[A <: Tuple]: List[Equal[_]] =
+    inline erasedValue[A] match {
+      case _: EmptyTuple => Nil
+      case _: (t *: ts)  => summonInline[Equal[t]] :: summonAll[ts]
+    }
 
-	private def equalSum[A](sum: Mirror.SumOf[A], elems: List[Equal[_]]): Equal[A] =
-		Equal.make { (left, right) =>
-			val leftOrdinal = sum.ordinal(left)
-			val rightOrdinal = sum.ordinal(right)
-			(leftOrdinal == rightOrdinal) && check(elems(leftOrdinal))(left, right)
-		}
+  private def equalSum[A](sum: Mirror.SumOf[A], elems: List[Equal[_]]): Equal[A] =
+    Equal.make { (left, right) =>
+      val leftOrdinal = sum.ordinal(left)
+      val rightOrdinal = sum.ordinal(right)
+      (leftOrdinal == rightOrdinal) && check(elems(leftOrdinal))(left, right)
+    }
 
-	private def equalProduct[A](product: Mirror.ProductOf[A], elems: List[Equal[_]]): Equal[A] =
-		Equal.make { (left, right) =>
-			iterator(left).zip(iterator(right)).zip(elems.iterator).forall {
-				case ((l, r), elem) => check(elem)(l, r)
-			}
-		}
+  private def equalProduct[A](product: Mirror.ProductOf[A], elems: List[Equal[_]]): Equal[A] =
+    Equal.make { (left, right) =>
+      iterator(left).zip(iterator(right)).zip(elems.iterator).forall {
+        case ((l, r), elem) => check(elem)(l, r)
+      }
+    }
 
-	private def check(equal: Equal[_])(left: Any, right: Any): Boolean =
-		equal.asInstanceOf[Equal[Any]].equal(left, right)
+  private def check(equal: Equal[_])(left: Any, right: Any): Boolean =
+    equal.asInstanceOf[Equal[Any]].equal(left, right)
 
-	private def iterator[A](a: A) =
-		a.asInstanceOf[Product].productIterator
+  private def iterator[A](a: A) =
+    a.asInstanceOf[Product].productIterator
 }
