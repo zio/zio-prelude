6e00866aa2b0ce15b1d0629104861ddf5b811de1
jules Ivanic <jules.ivanic@gmail.com>
2023-03-02 19:25:33 +0800

    Fix comment

--- core/shared/src/main/scala-2/zio/prelude/Newtype.scala
+++ core/shared/src/main/scala-2/zio/prelude/Newtype.scala
@@ -284,7 +284,7 @@ abstract class Newtype[A] extends NewtypeVersionSpecific {
    */
   def wrapAll[F[_]](value: F[A]): F[Type] = macro zio.prelude.Macros.wrapAll_impl[F, A, Type]
 
-  /**
+  /*
    * Must not be put as an implicit function.
    * Otherwise, the pattern matching on newtypes behaviour becomes incorrect.
    *
