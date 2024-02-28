cf347f83d366088a04fc8bf87f8fceb49a0331bc
Scala Steward <scala_steward@virtuslab.com>
2024-02-28 14:23:59 +0000

    Update scala-compiler, scala-library, ... to 2.12.19

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -8,7 +8,7 @@ import sbtcrossproject.CrossPlugin.autoImport.*
 import scalafix.sbt.ScalafixPlugin.autoImport.*
 
 object BuildHelper {
-  val Scala212: String = "2.12.18"
+  val Scala212: String = "2.12.19"
   val Scala213: String = "2.13.12"
   val Scala3: String   = "3.3.0"
 
