8de1e1c21e6312ef4900d53934c41365d6443629
johnsonjii <johnsonjii@potenciasoftware.com>
2024-05-08 14:18:05 -0600

    Fixed #1312 - Formatting

--- core-tests/shared/src/test/scala/zio/prelude/NonEmptySetSpec.scala
+++ core-tests/shared/src/test/scala/zio/prelude/NonEmptySetSpec.scala
@@ -88,7 +88,7 @@ object NonEmptySetSpec extends ZIOBaseSpec {
         test("removeNonEmpty") {
           check(genSet) { as =>
             val toRemove = as.head
-            val actual = NonEmptySet.fromSetOption(as).get.removeNonEmpty(toRemove).map(_.toSet)
+            val actual   = NonEmptySet.fromSetOption(as).get.removeNonEmpty(toRemove).map(_.toSet)
             val modified = as - toRemove
             val expected = Option(modified).filter(_.nonEmpty)
             actual <-> expected
