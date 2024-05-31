5a415706eb8cf40f01e5d3bfac7279c752720322
jules Ivanic <jules.ivanic@gmail.com>
2024-05-31 18:35:17 +0800

    Add test validating that passing a `Subtype[String]` and a `SubtypeCustom[String]` to a function asking for a `String` does compile

--- core-tests/shared/src/test/scala-3/zio/prelude/validators.scala
+++ core-tests/shared/src/test/scala-3/zio/prelude/validators.scala
@@ -6,3 +6,9 @@ object PalindromeValidator
     extends Validator[String](str =>
       if (str.reverse == str) Right(()) else Left(AssertionError.Failure("isPalindrome"))
     )
+
+object NonEmptyStringValidator
+    extends Validator[String]((s: String) =>
+      if (s.isBlank) Left(AssertionError.failure("NonEmptyString cannot be empty"))
+      else Right(())
+    )
