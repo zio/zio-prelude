dc5efd833eabb70e9969b9bbfca52dbfd3a2a615
Kyri Petrou <kyri.petrou@outlook.com>
2024-08-25 11:57:21 +0300

    Run `DistributiveProdSpec` suite sequentially

--- experimental-tests/shared/src/test/scala/zio/prelude/experimental/DistributiveProdSpec.scala
+++ experimental-tests/shared/src/test/scala/zio/prelude/experimental/DistributiveProdSpec.scala
@@ -4,6 +4,7 @@ package experimental
 import zio.prelude.experimental.laws._
 import zio.prelude.laws._
 import zio.test._
+import zio.test.TestAspect._
 import zio.test.laws._
 
 object DistributiveProdSpec extends ZIOBaseSpec {
@@ -15,5 +16,5 @@ object DistributiveProdSpec extends ZIOBaseSpec {
         test("ParSeq distributive multiply")(checkAllLaws(DistributiveProdLaws)(Gens.parSeq(Gen.unit, Gen.int))),
         test("fx.Cause distributive multiply")(checkAllLaws(DistributiveProdLaws)(Gens.parSeq(Gen.empty, Gen.int)))
       )
-    )
+    ) @@ exceptJVM(sequential)
 }
