3dc9c9e0e3498118822ca54517c968a5757aef49
Jules Ivanic <jules.ivanic@gmail.com>
2024-11-13 18:03:35 +1100

    Add `IdentityBoth` instance for `zio.prelude.data.Optional`

--- core/shared/src/main/scala/zio/prelude/AssociativeBoth.scala
+++ core/shared/src/main/scala/zio/prelude/AssociativeBoth.scala
@@ -18,6 +18,7 @@ package zio.prelude
 
 import zio._
 import zio.prelude.coherent.CovariantIdentityBoth
+import zio.prelude.data.Optional
 import zio.prelude.newtypes.{AndF, Failure, OrF}
 import zio.stm.ZSTM
 import zio.stream.{ZSink, ZStream}
@@ -1188,6 +1189,24 @@ object AssociativeBoth extends AssociativeBothLowPriority {
         (fa, fb) match {
           case (Some(a), Some(b)) => Some((a, b))
           case _                  => None
+        }
+    }
+
+  /**
+   * The [[IdentityBoth]] (with [[AssociativeBoth]]) instance for [[Optional]].
+   */
+  implicit val OptionalIdentityBoth: IdentityBoth[Optional] =
+    new IdentityBoth[Optional] {
+      val any: Optional[Any] = Optional.Present(())
+
+      def both[A, B](fa: => Optional[A], fb: => Optional[B]): Optional[(A, B)] =
+        fa match {
+          case Optional.Present(a) =>
+            fb match {
+              case Optional.Present(b) => Optional.Present((a, b))
+              case Optional.Absent     => Optional.Absent
+            }
+          case Optional.Absent     => Optional.Absent
         }
     }
 
