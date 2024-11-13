17fd13e839400cbc316e80498378a09cb44dc2f2
Jules Ivanic <jules.ivanic@gmail.com>
2024-11-13 18:40:51 +1100

    Add tests

--- core-tests/shared/src/test/scala/zio/prelude/IdentityFlattenSpec.scala
+++ core-tests/shared/src/test/scala/zio/prelude/IdentityFlattenSpec.scala
@@ -1,10 +1,18 @@
 package zio.prelude
 
+import zio.Trace
 import zio.prelude.laws._
+import zio.prelude.data.Optional
 import zio.test._
 import zio.test.laws._
 
 object IdentityFlattenSpec extends ZIOBaseSpec {
+
+  val optional: GenF[Any, Optional] =
+    new GenF[Any, Optional] {
+      def apply[R1, A](gen: Gen[R1, A])(implicit trace: Trace): Gen[R1, Optional[A]] =
+        Gen.option(gen)
+    }
 
   def spec: Spec[Environment, Any] =
     suite("IdentityFlattenSpec")(
@@ -13,6 +21,7 @@ object IdentityFlattenSpec extends ZIOBaseSpec {
         test("either")(checkAllLaws(IdentityFlattenLaws)(GenFs.either(Gen.int), Gen.int)),
         test("list")(checkAllLaws(IdentityFlattenLaws)(GenF.list, Gen.int)),
         test("option")(checkAllLaws(IdentityFlattenLaws)(GenF.option, Gen.int)),
+        test("optional")(checkAllLaws(IdentityFlattenLaws)(optional, Gen.int)),
         test("vector")(checkAllLaws(IdentityFlattenLaws)(GenF.vector, Gen.int))
       )
     )
