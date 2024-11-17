fac152b8feebf07af348c468a46adbc7f179c6c1
Jules Ivanic <jules.ivanic@gmail.com>
2024-11-17 13:17:45 +1100

    clean

--- core/shared/src/main/scala-2.13+/zio/prelude/IterableOnceCompat.scala
+++ core/shared/src/main/scala-2.13+/zio/prelude/IterableOnceCompat.scala
@@ -2,5 +2,7 @@ package zio.prelude
 
 /**
  * Needed because Scala 2.12 doesn't have `IterableOnce`
+ *
+ * TODO: Remove when support for Scala 2.12 is dropped
  */
 private[prelude] trait IterableOnceCompat[+A] extends IterableOnce[A]
